381ebed4b8522d1ee4db73c326ddbd7f0f605409
felix <1393491999@qq.com>
2023-07-14 17:37:48 +0800

    refactor: 美化控制台输出，添加容错判断

--- src/commands/init.js
+++ src/commands/init.js
@@ -3,6 +3,8 @@ import ora from 'ora';
 import download from 'download-git-repo';
 import path from 'path';
 import chalk from 'chalk';
+import fs from 'fs-extra';
+import logSymbols from 'log-symbols';
 
 export default (program) => {
   program.command('create <project>')
@@ -24,14 +26,32 @@ export default (program) => {
             'react-webpack'
           ]
         }
-      ]).then(answer => {
+      ]).then(async answer => {
         console.log('answer', answer);
-
-        const spinner = ora().start();
-        spinner.text = '模板项目正在下载中...';
 
         const downloadPath = path.join(process.cwd(), project);
         console.log(downloadPath);
+
+        // 判断用户创建的项目文件夹是否已经存在
+        if (fs.existsSync(downloadPath)) {
+          console.log(logSymbols.info, chalk.blue(`文件夹 ${project} 已经存在！`));
+          try {
+            // 若存在，则询问用户是否覆盖当前文件夹的内容，yes 则覆盖，no 则退出。
+            const { isCover } = await inquirer.prompt([
+              { name: 'isCover', message: '是否要覆盖当前文件夹的内容', type: 'confirm' }
+            ]);
+
+            if (!isCover) {
+              return;
+            }
+          } catch (error) {
+            console.log(logSymbols.fail,chalk.red('项目初始化失败，已退出!') );
+            return;
+          }
+        }
+
+        const spinner = ora().start();
+        spinner.text = '模板项目正在下载中...';
 
         let repository = 'fyiloutingfengyu/f-react-template';
 
