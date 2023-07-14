b60fd1c650ae5fb0e12b314dc2ae45eae09870f0
felix <1393491999@qq.com>
2023-07-14 16:35:24 +0800

    feat: 完成初版脚手架功能

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,10 +1,14 @@
 import inquirer from 'inquirer';
+import ora from 'ora';
+import download from 'download-git-repo';
+import path from 'path';
+import chalk from 'chalk';
 
 export default (program) => {
-  program.command('create <projectName>')
+  program.command('create <project>')
     .alias('init')
     .description('创建一个新项目')
-    .action(() => {
+    .action((project) => {
       inquirer.prompt([
         {
           name: 'author',
@@ -22,6 +26,31 @@ export default (program) => {
         }
       ]).then(answer => {
         console.log('answer', answer);
+
+        const spinner = ora().start();
+        spinner.text = '模板项目正在下载中...';
+
+        const downloadPath = path.join(process.cwd(), project);
+        console.log(downloadPath);
+
+        let repository = 'fyiloutingfengyu/f-react-template';
+
+        if (answer.template === 'react-webpack') {
+          repository = 'fyiloutingfengyu/f-react-template-webpack';
+        }
+
+        download(repository, downloadPath, (error) => {
+          if (!error) {
+            spinner.succeed(chalk.green.bold('模板项目下载成功！'));
+            console.log(chalk.blue.bold('项目已经准备就绪! 你可以运行：'));
+            console.log(chalk.yellow('cd ' + project));
+            console.log(chalk.yellow('npm install '));
+            console.log(chalk.yellow('npm run dev'));
+          } else {
+            spinner.fail('模板项目下载失败！');
+          }
+        });
+
       });
     });
 }
