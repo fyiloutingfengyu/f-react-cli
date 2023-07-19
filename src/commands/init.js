fb7923bccad74fea1f37ef752f32d49e75fed37c
felix <1393491999@qq.com>
2023-07-19 09:31:01 +0800

    refactor: 将用户提示提取到配置文件中

--- src/commands/init.js
+++ src/commands/init.js
@@ -2,38 +2,21 @@ import inquirer from 'inquirer';
 import ora from 'ora';
 import download from 'download-git-repo';
 import path from 'path';
-import chalk from 'chalk';
 import fs from 'fs-extra';
+import chalk from 'chalk';
 import logSymbols from 'log-symbols';
 import handlebars from 'handlebars';
 import templateConfig from '../config/template.js';
+import promptConfig from '../config/prompt.js';
 
 export default (program) => {
   program.command('create <projectName>')
     .alias('init')
     .description('创建一个新项目')
     .action((projectName) => {
-      inquirer.prompt([
-        {
-          name: 'author',
-          message: '你的名字是：',
-          type: 'input'
-        },
-        {
-          name: 'description',
-          message: '请输入项目描述：',
-          type: 'input'
-        },
-        {
-          name: 'template',
-          message: '请选择要使用的项目模板',
-          type: 'list',
-          choices: templateConfig.names
-        }
-      ]).then(async answer => {
-        console.log('answer', answer);
+      inquirer.prompt(promptConfig).then(async answer => {
         const { author, description, template } = answer;
-        const pakOptions = {
+        const packageOptions = {
           name: projectName,
           author,
           description
@@ -45,16 +28,20 @@ export default (program) => {
         if (fs.existsSync(downloadPath)) {
           console.log(logSymbols.info, chalk.blue(`文件夹 ${projectName} 已经存在！`));
           try {
-            // 若存在，则询问用户是否覆盖当前文件夹的内容，yes 则覆盖，no 则退出。
+            // 询问用户是否覆盖当前文件夹的内容，yes-覆盖，no-退出
             const { isCover } = await inquirer.prompt([
-              { name: 'isCover', message: '是否要覆盖当前文件夹的内容', type: 'confirm' }
+              {
+                name: 'isCover',
+                message: '是否要覆盖当前文件夹的内容',
+                type: 'confirm'
+              }
             ]);
 
             if (!isCover) {
               return;
             }
           } catch (error) {
-            console.log(logSymbols.fail, chalk.red('项目初始化失败，已退出!'));
+            console.log(logSymbols.error, chalk.red('项目初始化失败，已退出!'));
             return;
           }
         }
@@ -71,17 +58,17 @@ export default (program) => {
             if (fs.pathExistsSync(targetPackageFile)) {
               const content = fs.readFileSync(targetPackageFile, 'utf8');
               // 利用 handlebars 将用户输入的部分内容写入模板中
-              const result = handlebars.compile(content)(pakOptions);
+              const result = handlebars.compile(content)(packageOptions);
 
               fs.writeFileSync(targetPackageFile, result);
             } else {
-              console.log('package.json 文件不存在：' + targetPackageFile);
+              console.log(chalk.red(`package.json 文件不存在：${targetPackageFile}`));
             }
 
             spinner.succeed(chalk.green.bold('模板项目下载成功！'));
             console.log(chalk.blue.bold('项目已经准备就绪! 你可以运行：'));
             console.log(chalk.yellow(`cd ${projectName}`));
-            console.log(chalk.yellow('npm install '));
+            console.log(chalk.yellow('npm install'));
             console.log(chalk.yellow('npm run dev'));
           } else {
             spinner.fail('模板项目下载失败！');
